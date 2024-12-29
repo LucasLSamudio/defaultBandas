3416ced4077ac938488e9266a6a033ad69d55faa
Lucas Samudio <samudiolucas206@gmail.com>
2024-12-29 15:42:48 -0300

    first commit

---
+++ routes/bandasRoutes.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+const bandasController = require('../controller/bandasControllers')
+
+// Forma 1
+
+// const bandasController = require('../controller/bandasControllers')
+
+// Forma 2 (REACTIVAR)
+
+// const {listadoBandas} = require('../controller/bandasControllers')
+// router.get('/',listadoBandas);
+
+/* PRUEBAS */
+
+router.get('/', bandasController.index);
+router.get('/genero/:genero?', bandasController.porGenero);
+router.get('/id/:id', bandasController.porId);
+
+module.exports = router 
